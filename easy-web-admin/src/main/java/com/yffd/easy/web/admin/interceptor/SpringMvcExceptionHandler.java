b6510c01d5fb2ee06513c0306dc46b0addd394ef
yffd <yffd2014@sina.com>
2017-11-17 14:31:23 +0800

    admin init

--- easy-web-admin/src/main/java/com/yffd/easy/web/admin/interceptor/SpringMvcExceptionHandler.java
+++ easy-web-admin/src/main/java/com/yffd/easy/web/admin/interceptor/SpringMvcExceptionHandler.java
@@ -2,8 +2,6 @@ package com.yffd.easy.web.admin.interceptor;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
